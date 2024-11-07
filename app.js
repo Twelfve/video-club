c708ee953cfbb25198625e9cd567bd2603a6b4c5
Twelfve <victortyburon1212@gmail.com>
2024-11-06 23:43:45 -0600

    TempCommit

--- app.js
+++ app.js
@@ -1,63 +1,68 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+const { expressjwt } = require("express-jwt");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const directorsRouter = require('./routes/directors');
-const membersRouter = require('./routes/members');
+const jwtKey = "50afdb2b6f3a7faca28c57a036868247";
 
-const app = express();
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var directorsRouter = require("./routes/directors");
+var moviesRouter = require("./routes/movies");
+var membersRouter = require("./routes/members");
 
-//mongodb://<dbUser>?:<dbPassword>?@?<host>:<port>/<dbName>
-const url = 'mongodb://localhost:27017/video-club';
-mongoose.connect(url, 
-//   {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }
-);
+var app = express();
+
+const url = "mongodb://localhost:27017/video-club";
+mongoose.connect(url);
 const db = mongoose.connection;
 
-db.on('open', () => {
-  console.log('Connected to the database');
+db.on("open", () => {
+  console.log("Conexión a la base de datos establecida");
+  
 });
-db.on('error', (error) => {
-  console.log('Error connecting to the database', error);
+db.on("error", () => {
+  console.log("Error de conexión a la base de datos");
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/directors', directorsRouter);
-app.use('/members', membersRouter);
+app.use(
+  expressjwt({ secret: jwtKey, algorithms: ["HS256"] }).unless({
+    path: ["/login"],
+  })
+);
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/directors", directorsRouter);
+app.use("/movies", moviesRouter);
+app.use("/members", membersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,66 +1,55 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const User = require('../models/user.model');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 function home(req, res, next) {
-    res.render('index', { title: 'Express' });
+  res.render("index", { title: "Express" });
 }
 
 function login(req, res, next) {
-    
-    const { email, password } = req.body;
+  const email = req.body.email;
+  const password = req.body.password;
+  const jwtKey = "50afdb2b6f3a7faca28c57a036868247";
 
-    User.findOne({ "_email": email }).then((user) => {
-        if (user) {
-            bcrypt.hash(password, user.salt, function(err, hash) {
-                if(err){
-                    res.status(403).json({
-                        msg: 'Login failed',
-                        data: 'Invalid credentials'
-                    })
-                }
-                if (hash === user.password) {
-                    res.status(200).json({
-                        msg: 'Login success',
-                        data: user
-                    });
-                } else {
-                    res.status(401).json({
-                        msg: 'Login failed',
-                        data: 'Invalid credentials'
-                    });
-                }
-            });
-
-            bcrypt.compare(password, user.password, function(err, result) {
-                if (result) {
-                    res.status(200).json({
-                        msg: 'Login success',
-                        data: user
-                    });
-                } else {
-                    res.status(401).json({
-                        msg: 'Login failed',
-                        data: 'Invalid credentials'
-                    });
-                }
-            });
-        } else {
-            res.status(401).json({
-                msg: 'Login failed',
-                data: 'Invalid credentials'
-            });
+  User.findOne({ _email: email }).then((user) => {
+    if (user) {
+      bcrypt.hash(password, user.salt, (err, hash) => {
+        if (err) {
+          res.status(401).json({
+            msg: "Usuario y/o contraseña incorrectos",
+            obj: null,
+          });
         }
-    }).catch((error) => {
-        res.status(500).json({
-            msg: 'Error login',
-            data: error
-        });
-    });
 
+        if (hash === user.password) {
+          res.status(200).json({
+            msg: "Sesion iniciada correctamente",
+            obj: jwt.sign(
+              {
+                data: user.id,
+                exp: Math.floor(Date.now() / 1000) + 180,
+              },
+              jwtKey
+            ),
+          });
+        } else {
+          res.status(401).json({
+            msg: "Usuario y/o contraseña incorrectos",
+            obj: null,
+          });
+        }
+      });
+    } else {
+      res.status(401).json({
+        msg: "Usuario y/o contraseña incorrectos",
+        obj: null,
+      });
+    }
+  });
 }
 
 module.exports = {
-    home,
-    login
+  home,
+  login,
 };
